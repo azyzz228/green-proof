9617a8e41f53febb00d7a53121840ccbb49a9230
Aziz Abdullaev <as1036@duke.edu>
2023-04-22 04:10:14 -0400

    add routes

--- frontend/green-proof/src/App.jsx
+++ frontend/green-proof/src/App.jsx
@@ -1,10 +1,10 @@
+import Supplier from "./pages/supplier"
 
 function App() {
 
   return (
     <>
-      
-      <h1 className='text-4xl text-zinc-950 p-20'>Vite + React</h1>
+     <Supplier /> 
     </>
   )
 }
--- frontend/green-proof/src/main.jsx
+++ frontend/green-proof/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />}></Route>
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
+);
